197fa7178b981679f58ab99575fbbc300faa3f1b
Marcin Gardas <mkgardas@gmail.com>
2020-06-07 17:25:02 +0100

    Show data from `users-module` in `user-module`

--- packages/module-user/src/components/App/index.tsx
+++ packages/module-user/src/components/App/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
 import * as styles from './index.module.css'
 import { UrlService, UrlChangeListener, APIService } from '@marcingardas/communication'
+
 import { ModuleUsersAPIInterface } from '../../api/ModuleUsersAPIInterface'
 
 type Props = {};
 type State = {
     visible: boolean,
+    userId: string | null,
 };
 
 class App extends React.Component<Props, State> {
@@ -14,14 +16,17 @@ class App extends React.Component<Props, State> {
 
         this.state = {
             visible: false,
+            userId: null,
         }
     }
 
     componentDidMount() {
         const urlChangeListener: UrlChangeListener = (url: string): void => {
             if (url.search(/users\/\d+/i) >= 0) {
+                const userId = url.substring(url.lastIndexOf('/') + 1)
                 this.setState({
                     visible: true,
+                    userId,
                 });
 
                 return;
@@ -29,6 +34,7 @@ class App extends React.Component<Props, State> {
 
             this.setState({
                 visible: false,
+                userId: null,
             });
         }
 
@@ -36,7 +42,7 @@ class App extends React.Component<Props, State> {
     }
 
     render() {
-        const { visible } = this.state;
+        const { visible, userId } = this.state;
 
         if (!visible) {
             return <></>;
@@ -45,13 +51,20 @@ class App extends React.Component<Props, State> {
         const moduleUsersAPI: ModuleUsersAPIInterface = new APIService().get('users') as ModuleUsersAPIInterface
         const users = moduleUsersAPI.getUsers()
 
+        const user = users.find((user) => user.id === userId)
+
         return (
             <div className={styles.wrapper}>
                 <div className={styles.innerWrapper}>
                     <div className={styles.text}>
-                        This is the user module
+                        This is the users data from <b>module-users</b>:<br />
+                        {JSON.stringify(users)}
+                    </div>
 
-                        This is the data I got form users module: {JSON.stringify(users)}
+                    <div className={styles.text}>
+                        This is the data of the current user:<br />
+                        Id: <b>{user.id}</b><br />
+                        Name: <b>{user.name}</b>
                     </div>
                 </div>
             </div>
