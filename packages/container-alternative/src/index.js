8f182b9240eb0dcf25d1cc77955fa6ffe8813cc4
Marcin Gardas <mkgardas@gmail.com>
2020-06-08 23:49:34 +0100

    Clean up container-alternative project

--- packages/container-alternative/src/index.js
+++ packages/container-alternative/src/index.js
@@ -1,21 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import '@marcingardas/module-navigation';
 import '@marcingardas/module-settings';
 import '@marcingardas/module-user';
 import '@marcingardas/module-users';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
